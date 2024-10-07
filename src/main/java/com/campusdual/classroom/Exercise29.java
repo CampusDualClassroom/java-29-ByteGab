015a1a57483820a21318f16092c4d45ebb22fb57
Bytegab <gabrielfreiresimon@gmail.com>
2024-10-07 11:18:15 +0200

    Ejercicio29 completado

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,13 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try
+        {
+            int erroInt = 10 / 0;
+        }
+        catch (ArithmeticException exception)
+        {
+            System.out.println("Error" + exception.getMessage());
+        }
     }
 }
